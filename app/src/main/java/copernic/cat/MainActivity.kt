b0f2929519b01decea733a7a332cc1afda51ab94
= <=>
2022-10-31 18:44:09 +0100

    Pantallas final marc

--- app/src/main/java/copernic/cat/MainActivity.kt
+++ app/src/main/java/copernic/cat/MainActivity.kt
@@ -1,12 +1,14 @@
 package copernic.cat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
         setContentView(R.layout.activity_main)
+
     }
 }
